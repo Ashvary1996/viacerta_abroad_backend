aea29c4d1cd9c5ada6f3e5942488f57837b31c32
Ashvary Gidian <ashvarygidian1996@gmail.com>
2025-03-12 21:58:06 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config({ path: "./config/.env" });
+const connectToDb = require("./dbConnector");
+// const { sendToken } = require("./utils/jwtToken");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// .................................................................
+const allowedOrigins = ["http://localhost:3000", process.env.CORS_ORIGIN];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// .................................................................
+
+// app.use("/demo", (req, res) => res.send("Hello World"));
+
+// app.use("/user", require("./routes/UserRoute"));
+// app.use("/home", require("./routes/HomeRoute"));
+// app.use("/product", require("./routes/ProductRoute"));
+// app.use("/user/cart", require("./routes/CartRoute"));
+// app.use("/order", require("./routes/OrderRoute"));
+
+// .................................................................
+
+app.listen(port, () => console.log(`App listening on port: ${port}!`));
+connectToDb();
+
+// .................................................................
+module.exports = app;
---
+++ models/users.js
@@ -0,0 +1,71 @@
+import mongoose, { Schema } from "mongoose";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name"],
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+      unique: true,
+      lowercase: true,
+        validate: {
+          validator: validator.isEmail,
+          message: "Not a valid email address",
+        },
+      index: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password Required"],
+    },
+    mobile: {
+      type: String,
+      required: [true, "Please enter your phone number"],
+      unique: true,
+        validate: {
+          validator: function (value) {
+            return /^\d{10}$/.test(value);
+          },
+          message: "Phone number must be 10 digits",
+        },
+    },
+    address: {
+      pinCode: { type: String },
+      city: { type: String },
+      state: { type: String },
+      country: { type: String },
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+
+    favouriteCourse: {
+      type: [Schema.Types.ObjectId],
+      ref: "courses",
+    },
+    otp: {
+      type: String,
+    },
+
+    otpExpiry: {
+      type: Date,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("users", userSchema);
+
+export default User;
